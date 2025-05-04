37b6c14e2b645fad989e5a1a3fc557422e0c5282
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-05-04 08:25:04 +0600

    TheDragonNews register done.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
     return (
         <div className='flex justify-center min-h-screen items-center'>
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
-                <h2 className='font-semibold text-2xl text-center'>Login Your Account</h2>
+                <h2 className='font-semibold text-2xl text-center'>Login your account</h2>
                 <div className="card-body">
                     <fieldset className="fieldset">
                         <label className="label font-semibold">Email</label>
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Register = () => {
     return (
         <div>
-            
+            <div className='flex justify-center min-h-screen items-center'>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
+                    <h2 className='font-semibold text-2xl text-center'>Register your account</h2>
+                    <div className="card-body">
+                        <fieldset className="fieldset">
+                            <label className="label font-semibold">Name</label>
+                            <input type="text" className="input" placeholder="Enter your name" />
+                            <label className="label font-semibold">Photo URl</label>
+                            <input type="text" className="input" placeholder="Enter photo URL" />
+                            <label className="label font-semibold">Email</label>
+                            <input type="email" className="input" placeholder="Email" />
+                            <label className="label font-semibold">Password</label>
+                            <input type="password" className="input" placeholder="Password" />
+                            <div></div>
+                            <button className="btn btn-neutral mt-4">Login</button>
+                        </fieldset>
+
+                        <p className='text-center pt-3 font-semibold'>Already have an account? <Link to="/auth/login" className='text-secondary link-hover'>Login</Link></p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
